d6289aee47a3c6ad7e395bfeba1d41fb788c0e0f
Oleh Titov <oleg.titov81@gmail.com>
2022-10-28 22:33:08 +0200

    get rid of MVVM pattern in ConfigureJar view

--- JarTest/View/ConfigureJar.swift
+++ JarTest/View/ConfigureJar.swift
@@ -9,13 +9,19 @@ import SwiftUI
 import SwiftUIPager
 
 struct ConfigureJar: View {
-    @StateObject var vm = ViewModel()
+    @State var nextView: Bool = false
+    @State var selectedCurrency: ForeignCurrency = .eur
+    @State var name: String = "For "
     var body: some View {
         VStack {
-            if vm.nextView {
-                SetAmount(selectedCurrency: vm.selectedCurrency, jarName: vm.name)
+            if nextView {
+                SetAmount(
+                    selectedCurrency: selectedCurrency,
+                    jarName: name)
             } else {
-                Content(vm: vm)
+                Content(
+                    selectedCurrency: $selectedCurrency,
+                    name: $name, nextView: $nextView)
             }
         }
     }
@@ -48,22 +54,26 @@ struct ConfigureJar_Previews: PreviewProvider {
 
 extension ConfigureJar {
     struct Content: View {
-        @StateObject var vm : ViewModel
+        @Binding var selectedCurrency: ForeignCurrency
+        @Binding var name: String
+        @State var page: Page = .first()
+        @State var isFirstResponder : Bool = true
+        @Binding var nextView: Bool
         
-        let headline = "What are you saving for?"
-        let buttonText = "Create Jar"
+        //Make it a static let in ForeignCurrency
+        var currencies: [ForeignCurrency] = [.usd, .eur, .pln]
+        
         let jarImageName = "Jar-4"
         let pagerItemSpacing : CGFloat = 0
         let pagerInteractiveScale : Double = 0.7
         let pagerItemAspectRatio : Double = 0.8
-//        let headlineFont = Font.custom("RobotoMono-Bold", size: 20)
         
         var body: some View {
             ZStack {
                 BackgroundView()
                 VStack(spacing: 12) {
                     ZStack {
-                        Pager(page: vm.page, data: vm.currencies, id: \.self) { item in
+                        Pager(page: page, data: currencies, id: \.self) { item in
                             self.pageView(item)
                                 .padding()
                         }
@@ -74,25 +84,25 @@ extension ConfigureJar {
                         .draggingAnimation(.interactive)
                         .itemAspectRatio(pagerItemAspectRatio, alignment: .center)
                         .onPageChanged({ (newIndex) in
-                            vm.updateSelectedCurrencyWith(newIndex: newIndex)
+                            updateSelectedCurrencyWith(newIndex: newIndex)
                         })
                         .onAppear {
-                            vm.scrollPagerToNextOption()
+                            scrollPagerToNextOption()
                         }
                     }
                         
                     VStack(spacing: 12) {
-                        Text(headline)
+                        Text("What are you saving for?")
                             .font(.customHeadlineFont)
                         LegacyTextField(
-                            text: $vm.name,
-                            isFirstResponder: $vm.isFirstResponder,
+                            text: $name,
+                            isFirstResponder: $isFirstResponder,
                             keyboard: .alphabet
                         )
                         Spacer()
-                        Button(action: {vm.nextPressed()}, label: {})
-                            .disabled(vm.checkButtonDisabled())
-                            .buttonStyle(MyButtonStyle(title: buttonText))
+                        Button(action: {nextPressed()}, label: {})
+                            .disabled(checkButtonDisabled())
+                            .buttonStyle(MyButtonStyle(title: "Create Jar"))
                             .padding(.bottom)
                     }
                 }
@@ -125,26 +135,13 @@ extension ConfigureJar {
                 Spacer()
             }
         }
-    }
-}
-
-extension ConfigureJar {
-    class ViewModel : ObservableObject {
-        @Published var selectedCurrency = ForeignCurrency.eur
-        @Published var name = "For "
-        @Published var page: Page = .first()
-        @Published var isFirstResponder : Bool = true
-        @Published var nextView: Bool = false
-        
-        var currencies: [ForeignCurrency] = [.usd, .eur, .pln]
-        
-        func checkButtonDisabled() -> Bool {
-            let trimmed = name.trimmingCharacters(in: .whitespacesAndNewlines)
-            return trimmed.isEmpty || trimmed == "For" || trimmed == "Fo" || trimmed == "F"
-        }
         
         func updateSelectedCurrencyWith(newIndex : Int) {
             selectedCurrency = currencies[newIndex]
+        }
+        
+        func scrollPagerToNextOption() {
+            self.page.update(.next)
         }
         
         func nextPressed() {
@@ -153,10 +150,9 @@ extension ConfigureJar {
             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
         
-        func scrollPagerToNextOption() {
-            self.page.update(.next)
+        func checkButtonDisabled() -> Bool {
+            let trimmed = name.trimmingCharacters(in: .whitespacesAndNewlines)
+            return trimmed.isEmpty || trimmed == "For" || trimmed == "Fo" || trimmed == "F"
         }
     }
 }
-
-
